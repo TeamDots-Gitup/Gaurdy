088dc160849642dacac9bf30ffe6cf4ff8128a42
s92061078 <90457529+s92061078@users.noreply.github.com>
2023-01-10 11:46:20 +0530

    Get started, Login, Password Reset, Search station,

---
+++ screens/PasswordReset.js
@@ -0,0 +1,293 @@
+import * as React from "react";
+import { StyleSheet, View, Image, Text, Pressable } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import GlobalStyles from "../GlobalStyles";
+
+const PasswordReset = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.passwordReset}>
+      <View
+        style={[styles.iphoneXhomeIndicatorhomeI, styles.backgroundPosition]}
+      >
+        <View style={styles.iphoneXhomeIndicatorhomeIChild} />
+      </View>
+      <View style={styles.bars1StatusBarIphone}>
+        <View
+          style={[
+            styles.background,
+            styles.iconLayout,
+            styles.backgroundPosition,
+          ]}
+        />
+        <Image
+          style={styles.batteryIcon}
+          resizeMode="cover"
+          source={require("../assets/battery.png")}
+        />
+        <Image
+          style={styles.wifiIcon}
+          resizeMode="cover"
+          source={require("../assets/wifi4.png")}
+        />
+        <Image
+          style={styles.mobileSignalIcon}
+          resizeMode="cover"
+          source={require("../assets/mobile-signal.png")}
+        />
+        <View style={styles.timeStyle}>
+          <Text style={styles.text}>9:41</Text>
+        </View>
+      </View>
+      <Pressable
+        style={[styles.loginButtons, styles.anjaFlexBox]}
+        onPress={() => navigation.navigate("LogIn")}
+      >
+        <Text style={[styles.copy, styles.copyTypo]}>Reset Password</Text>
+      </Pressable>
+      <View style={[styles.passwordFormField, styles.passwordShadowBox]}>
+        <Text style={[styles.anja, styles.anjaFlexBox]}>
+          Confirm new Password
+        </Text>
+        <Image
+          style={styles.teenyIconEyeClosed}
+          resizeMode="cover"
+          source={require("../assets/teeny-icon--eyeclosed2.png")}
+        />
+      </View>
+      <View style={[styles.passwordFormField1, styles.passwordShadowBox]}>
+        <Text style={[styles.anja, styles.anjaFlexBox]}>New Password</Text>
+        <Image
+          style={styles.teenyIconEyeClosed}
+          resizeMode="cover"
+          source={require("../assets/teeny-icon--eyeclosed2.png")}
+        />
+      </View>
+      <View style={[styles.passwordFormField2, styles.passwordShadowBox]}>
+        <Text style={[styles.anja, styles.anjaFlexBox]}>Email</Text>
+        <Image
+          style={styles.teenyIconEyeClosed}
+          resizeMode="cover"
+          source={require("../assets/teeny-icon--eyeclosed2.png")}
+        />
+      </View>
+      <Image
+        style={styles.gaurdyLogoRemovebgPreview2Icon}
+        resizeMode="cover"
+        source={require("../assets/gaurdy-logoremovebgpreview-2.png")}
+      />
+      <Pressable
+        style={styles.arrowarrowLeft}
+        onPress={() => navigation.navigate("LogIn")}
+      >
+        <Image
+          style={[styles.icon, styles.iconLayout]}
+          resizeMode="cover"
+          source={require("../assets/arrowarrowleft.png")}
+        />
+      </Pressable>
+      <Text style={[styles.resetPassword, styles.copyTypo]}>
+        Reset Password
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundPosition: {
+    left: "0%",
+    bottom: "0%",
+    right: "0%",
+    position: "absolute",
+  },
+  iconLayout: {
+    height: "100%",
+    width: "100%",
+  },
+  anjaFlexBox: {
+    alignItems: "center",
+    position: "absolute",
+  },
+  copyTypo: {
+    fontFamily: GlobalStyles.FontFamily.b,
+    fontWeight: "700",
+    textAlign: "center",
+  },
+  passwordShadowBox: {
+    width: 327,
+    elevation: 12,
+    shadowRadius: 12,
+    shadowColor: "rgba(28, 37, 44, 0.05)",
+    backgroundColor: GlobalStyles.Color.blackModeSecondaryDark1,
+    height: 56,
+    shadowOpacity: 1,
+    shadowOffset: {
+      width: 0,
+      height: 14,
+    },
+    borderRadius: GlobalStyles.Border.br_xs,
+    left: 21,
+    position: "absolute",
+  },
+  iphoneXhomeIndicatorhomeIChild: {
+    marginLeft: -66.5,
+    bottom: 9,
+    left: "50%",
+    borderRadius: GlobalStyles.Border.br_xl,
+    backgroundColor: GlobalStyles.Color.devider,
+    width: 134,
+    height: 5,
+    position: "absolute",
+  },
+  iphoneXhomeIndicatorhomeI: {
+    height: "4.19%",
+    top: "95.81%",
+    width: "100%",
+  },
+  background: {
+    top: "0%",
+  },
+  batteryIcon: {
+    marginTop: -4,
+    right: 15,
+    width: 25,
+    height: 11,
+    top: "50%",
+    position: "absolute",
+  },
+  wifiIcon: {
+    width: 15,
+    height: 11,
+  },
+  mobileSignalIcon: {
+    width: 17,
+    height: 11,
+  },
+  text: {
+    marginTop: -9,
+    fontSize: GlobalStyles.FontSize.size_sm,
+    letterSpacing: 0,
+    fontWeight: "900",
+    fontFamily: GlobalStyles.FontFamily.sourceSansPro,
+    width: 54,
+    textAlign: "center",
+    color: GlobalStyles.Color.sUNRISEWhite1,
+    top: "50%",
+    left: 0,
+    position: "absolute",
+  },
+  timeStyle: {
+    height: "40.91%",
+    width: "14.4%",
+    top: "31.82%",
+    right: "80%",
+    bottom: "27.27%",
+    left: "5.6%",
+    position: "absolute",
+    overflow: "hidden",
+  },
+  bars1StatusBarIphone: {
+    top: 0,
+    width: 375,
+    height: 44,
+    left: 0,
+    position: "absolute",
+    overflow: "hidden",
+  },
+  copy: {
+    fontSize: GlobalStyles.FontSize.b_size,
+    lineHeight: 22,
+    color: GlobalStyles.Color.gray_700,
+  },
+  loginButtons: {
+    top: 696,
+    backgroundColor: GlobalStyles.Color.gold_100,
+    shadowColor: "rgba(28, 37, 44, 0.08)",
+    shadowRadius: 23,
+    elevation: 23,
+    width: 326,
+    flexDirection: "row",
+    paddingHorizontal: GlobalStyles.Padding.padding_md,
+    paddingVertical: GlobalStyles.Padding.padding_sm,
+    justifyContent: "center",
+    height: 56,
+    shadowOpacity: 1,
+    shadowOffset: {
+      width: 0,
+      height: 14,
+    },
+    borderRadius: GlobalStyles.Border.br_xs,
+    left: 21,
+    alignItems: "center",
+    overflow: "hidden",
+  },
+  anja: {
+    marginTop: -27,
+    left: 16,
+    fontSize: GlobalStyles.FontSize.mP_size,
+    lineHeight: 20,
+    fontFamily: GlobalStyles.FontFamily.mP,
+    color: GlobalStyles.Color.unselected,
+    textAlign: "left",
+    display: "flex",
+    width: 246,
+    height: 54,
+    top: "50%",
+  },
+  teenyIconEyeClosed: {
+    marginTop: -10,
+    right: 16,
+    width: 20,
+    height: 20,
+    top: "50%",
+    position: "absolute",
+  },
+  passwordFormField: {
+    top: 569,
+  },
+  passwordFormField1: {
+    top: 478,
+  },
+  passwordFormField2: {
+    top: 387,
+  },
+  gaurdyLogoRemovebgPreview2Icon: {
+    top: 44,
+    left: 5,
+    width: 370,
+    height: 236,
+    position: "absolute",
+  },
+  icon: {
+    overflow: "hidden",
+  },
+  arrowarrowLeft: {
+    left: 13,
+    top: 56,
+    width: 24,
+    height: 24,
+    position: "absolute",
+  },
+  resetPassword: {
+    top: 285,
+    left: 62,
+    fontSize: GlobalStyles.FontSize.size_lg,
+    lineHeight: 25,
+    width: 244,
+    height: 42,
+    color: GlobalStyles.Color.sUNRISEWhite1,
+    fontFamily: GlobalStyles.FontFamily.b,
+    fontWeight: "700",
+    position: "absolute",
+  },
+  passwordReset: {
+    backgroundColor: GlobalStyles.Color.gray_700,
+    flex: 1,
+    height: 812,
+    overflow: "hidden",
+    width: "100%",
+  },
+});
+
+export default PasswordReset;
