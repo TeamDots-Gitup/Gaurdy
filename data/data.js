c10f0d4e414bde22bb6ebc5711a4327a03425106
s92061078 <90457529+s92061078@users.noreply.github.com>
2023-12-07 00:49:07 +0530

    Create data.js
    slider bar datas

---
+++ data/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    img: require('../assets/sand.png'),
+    title: 'New rule alert',
+    description: 'Government has increased the speeding fine 2X from today midnight'
+  },
+  {
+    id: 2,
+    img: require('../assets/emer.webp'),
+    title: 'Emergency Notice',
+    description: 'Please be aware of emergency evacuation procedures in your area. Stay safe!'
+  },
+  {
+    id: 3,
+    img: require('../assets/public.webp'),
+    title: 'Public Gathering Reminder',
+    description: 'Large public gatherings require prior authorization. Ensure compliance for a smooth event.'
+  },
+  {
+    id: 4,
+    img: require('../assets/beach.webp'),
+    title: 'Beach Safety Advisory',
+    description: "Swim only in designated areas. Follow lifeguard instructions for a safe beach experience."
+  },
+  {
+    id: 5,
+    img: require('../assets/city.webp'),
+    title: 'Citywide Celebrations',
+    description: 'Join us for citywide celebrations on the main square this weekend. Fun activities for everyone!'
+  },
+];
